ca33a7825afcc621e6dd9fa21ff63dd43bb9da96
rubengglez <rubengglez@gmail.com>
2023-11-17 20:04:48 +0100

    feat: implement get and set commands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use std::net::{SocketAddr, TcpListener};
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader, BufWriter, Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+};
 
 use crate::config::Config;
 
 mod config;
 
+struct Item {
+    flags: u16,
+    exptime: usize,
+    value: String,
+    value_length: usize,
+}
+
+impl Item {
+    fn new(flags: u16, exptime: usize, value_length: usize, value: String) -> Self {
+        Item {
+            flags,
+            exptime,
+            value_length,
+            value,
+        }
+    }
+}
+
+type Store = HashMap<String, Item>;
 
 fn main() {
     let config = match Config::parse(std::env::args()) {
@@ -13,9 +36,80 @@ fn main() {
 
     let listener = TcpListener::bind(SocketAddr::from(([127, 0, 0, 1], config.port))).unwrap();
 
-    for stream in listener.incoming() {
-        let _ = stream.unwrap();
+    let mut store = HashMap::new();
 
-        println!("Connection established!");
+    for stream_wrapper in listener.incoming() {
+        let stream = stream_wrapper.unwrap();
+
+        handle_connection(stream, &mut store);
+    }
+}
+
+fn response_wrong_number_of_arguments(writer: &mut BufWriter<&TcpStream>, command: &str) {
+    response(writer, &format!("wrong number of arguments for {command}\r\n"));
+}
+
+fn response(writer: &mut BufWriter<&TcpStream>, response: &str) {
+    writer.write_all(response.as_bytes()).unwrap();
+    writer.flush().unwrap();
+}
+
+fn handle_connection(stream: TcpStream, store: &mut Store) {
+    loop {
+        let mut read_buffer = BufReader::new(&stream);
+        let mut write_buffer = BufWriter::new(&stream);
+
+        let mut command = String::new();
+        read_buffer.read_line(&mut command).unwrap();
+        if command.starts_with("set") {
+            let mut iterator = command.split_whitespace();
+            // TODO: is there a better way without cloning the iterator
+            let size = iterator.clone().count();
+            if size != 5 && size != 6 {
+                response_wrong_number_of_arguments(&mut write_buffer, "set");
+                continue;
+            }
+
+            iterator.next();
+            let key = String::from(iterator.next().unwrap());
+            let flags: u16 = iterator.next().unwrap().parse().unwrap();
+            let exptime: usize = iterator.next().unwrap().parse().unwrap();
+            let value_size_in_bytes: usize = iterator.next().unwrap().parse().unwrap();
+            let no_reply = iterator.next();
+
+            // TODO: validate size of payload with the value_size_in_bytes
+            let mut value = String::new();
+            read_buffer.read_line(&mut value).unwrap();
+
+            store.insert(key, Item::new(flags, exptime, value_size_in_bytes, value));
+
+            if no_reply == None {
+                response(&mut write_buffer, "STORED\r\n");
+            }
+        } else {
+            let mut iterator = command.split_whitespace();
+            // TODO: is there a better way without cloning the iterator
+            let size = iterator.clone().count();
+            if size != 2 {
+                response_wrong_number_of_arguments(&mut write_buffer, "set");
+                continue;
+            }
+            iterator.next();
+            let key = iterator.next().unwrap();
+
+            match store.get(key) {
+                None => {
+                    response(&mut write_buffer, "END\r\n");
+                }
+                Some(item) => {
+                    let mut message =
+                        format!("VALUE {} {} {}\r\n", key, item.flags, item.value_length);
+                    message += &item.value;
+                    message += "END\r\n";
+
+                    response(&mut write_buffer, &message);
+                }
+            }
+        }
     }
 }
